e1ba04195c362637d4a3c5cf585e9cc5f33ee6f9
G-jiaoyan <1279466690@qq.com>
2018-12-29 13:21:32 +0800

    jiaoyan

--- src/main/java/com/example/music_cloud/view/activity/MainActivity.java
+++ src/main/java/com/example/music_cloud/view/activity/MainActivity.java
@@ -2,6 +2,7 @@ package com.example.music_cloud.view.activity;
 
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.widget.Toast;
 
 import com.example.music_cloud.R;
 
@@ -12,5 +13,6 @@ public class MainActivity extends AppCompatActivity {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
 
+        Toast.makeText(this, "小姣姣提交", Toast.LENGTH_SHORT).show();
     }
 }
